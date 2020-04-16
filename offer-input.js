dc47f12d6a0e0b1ea843b620abbfdf4f965a1f9b
jumpinjuniper <47693543+jumpinjuniper@users.noreply.github.com>
2020-04-15 22:20:14 -0230

    Update offer-input.js

--- offer-input.js
+++ offer-input.js
@@ -1,58 +1,60 @@
-$('input[type="checkbox"]').click(function(source) {
-      if (this.getAttribute('data-name') == 'Ingredients') {
+    $('input[type="checkbox"]').click(function(source) {
+      if (this.getAttribute('question')) {
+        if (this.getAttribute('data-name') == 'Ingredients') {
+          if (this.checked) {
+            // Unhide Ingredient Question Component
+            document.getElementById("ingredients-container").style = "display: -webkit-box;\
+                                                                    display: -webkit-flex;\
+                                                                    display: -ms-flexbox;\
+                                                                    display: flex;\
+                                                                    margin-top: 15px;\
+                                                                    margin-bottom: 15px;\
+                                                                    padding: 20px;\
+                                                                    -webkit-box-orient: vertical;\
+                                                                    -webkit-box-direction: normal;\
+                                                                    -webkit-flex-direction: column;\
+                                                                    -ms-flex-direction: column;\
+                                                                    flex-direction: column;\
+                                                                    border-style: solid;\
+                                                                    border-width: 1px;\
+                                                                    border-color: #ccc;\
+                                                                    border-radius: 10px;"
+          } else {
+            // Hide Ingredient Question Component and Uncheck all checkboxs
+            var container = document.querySelector("#ingredients-container");
+            var checkboxesAll = container.querySelectorAll('input[type="checkbox"]');
+            checkboxesAll.forEach(checkbox => {
+              if (checkbox.checked) {
+                checkbox.click()
+              }
+            });
+            document.getElementById("ingredients-container").style = "display: none;"
+            
+          }
+      
+      } else {
+        var divId = this.getAttribute('name')
         if (this.checked) {
-          // Unhide Ingredient Question Component
-          document.getElementById("ingredients-container").style = "display: -webkit-box;\
-                                                                  display: -webkit-flex;\
-                                                                  display: -ms-flexbox;\
-                                                                  display: flex;\
-                                                                  margin-top: 15px;\
-                                                                  margin-bottom: 15px;\
-                                                                  padding: 20px;\
-                                                                  -webkit-box-orient: vertical;\
-                                                                  -webkit-box-direction: normal;\
-                                                                  -webkit-flex-direction: column;\
-                                                                  -ms-flex-direction: column;\
-                                                                  flex-direction: column;\
-                                                                  border-style: solid;\
-                                                                  border-width: 1px;\
-                                                                  border-color: #ccc;\
-                                                                  border-radius: 10px;"
-        } else {
-          // Hide Ingredient Question Component and Uncheck all checkboxs
-          var container = document.querySelector("#ingredients-container");
-          var checkboxesAll = container.querySelectorAll('input[type="checkbox"]');
-          checkboxesAll.forEach(checkbox => {
-            if (checkbox.checked) {
-              checkbox.click()
-            }
-          });
-          document.getElementById("ingredients-container").style = "display: none;"
-          
-        }
-    
-    } else {
-      var divId = this.getAttribute('name')
-      if (this.checked) {
-          // Unhide Ingredient Question Component
-          document.getElementById(divId).style = "display: -webkit-box;\
-                                                  display: -webkit-flex;\
-                                                  display: -ms-flexbox;\
-                                                  display: flex;\
-                                                  margin-top: 15px;\
-                                                  margin-bottom: 15px;\
-                                                  padding: 20px;\
-                                                  -webkit-box-orient: vertical;\
-                                                  -webkit-box-direction: normal;\
-                                                  -webkit-flex-direction: column;\
-                                                  -ms-flex-direction: column;\
-                                                  flex-direction: column;\
-                                                  border-style: solid;\
-                                                  border-width: 1px;\
-                                                  border-color: #ccc;\
-                                                  border-radius: 10px;"
-        } else {
-          document.getElementById(divId).style = "display: none;"
+            // Unhide Ingredient Question Component
+            document.getElementById(divId).style = "display: -webkit-box;\
+                                                    display: -webkit-flex;\
+                                                    display: -ms-flexbox;\
+                                                    display: flex;\
+                                                    margin-top: 15px;\
+                                                    margin-bottom: 15px;\
+                                                    padding: 20px;\
+                                                    -webkit-box-orient: vertical;\
+                                                    -webkit-box-direction: normal;\
+                                                    -webkit-flex-direction: column;\
+                                                    -ms-flex-direction: column;\
+                                                    flex-direction: column;\
+                                                    border-style: solid;\
+                                                    border-width: 1px;\
+                                                    border-color: #ccc;\
+                                                    border-radius: 10px;"
+          } else {
+            document.getElementById(divId).style = "display: none;"
+          }
         }
       }
     })
